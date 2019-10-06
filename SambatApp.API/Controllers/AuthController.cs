17bd7e86d05d66fc3d0d807f027220520b0372b3
hamimt <hamim.email@gmail.com>
2019-10-06 11:30:46 +0700

    Add Auth Repository, Registration Module

---
+++ SambatApp.API/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SambatApp.API.Data;
+using SambatApp.API.Dtos;
+using SambatApp.API.Models;
+
+namespace SambatApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            // validAte request
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+
+            if(await _repo.UserExists(userForRegisterDto.Username))
+            {
+                return BadRequest("Username already exist!");
+            }
+
+            var userToCreate = new User
+            {
+                Username = userForRegisterDto.Username
+            };
+
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+
+            return StatusCode(201);
+        }
+    }
+}
