44459b4a62ca6c95a99e41b0f1b45efe5b26bb66
hamimt <hamim.email@gmail.com>
2019-11-02 18:42:06 +0700

    create login page and service

--- SambatApp-SPA/src/app/app.module.ts
+++ SambatApp-SPA/src/app/app.module.ts
@@ -1,22 +1,31 @@
+import { AuthService } from './_services/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
+import { LoginformComponent } from './loginform/loginform.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
+      ValueComponent,
+      NavComponent,
+      LoginformComponent
    ],
    imports: [
       BrowserModule,
       AppRoutingModule,
-      HttpClientModule // digunakan untuk consume service di client
+      HttpClientModule, //digunakanuntukconsumeservicediclient
+      FormsModule
    ],
-   providers: [],
+   providers: [
+      AuthService
+   ],
    bootstrap: [
       AppComponent
    ]
---
+++ SambatApp-SPA/src/app/loginform/loginform.component.ts
@@ -0,0 +1,28 @@
+import { AuthService } from './../_services/auth.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loginform',
+  templateUrl: './loginform.component.html',
+  styleUrls: ['./loginform.component.css']
+})
+export class LoginformComponent implements OnInit {
+
+  model: any = {};
+
+  constructor(private authSrvice: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login(){
+    this.authSrvice.login(this.model).subscribe(
+    next => {
+      console.log('Signed in successfully');
+    },
+    error => {
+      console.log('Failed to signed in');
+    }
+    );
+  }
+}
